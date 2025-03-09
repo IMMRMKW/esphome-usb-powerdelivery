cd63a0f7d168fa83fb4e328094243668ddb2cd1d
IMMRMKW <marcel_welleweerd@hotmail.com>
2025-03-09 18:58:09 +0100

    changed files for files from ventimate

--- components/fusb302/fusb302.cpp
+++ components/fusb302/fusb302.cpp
@@ -1,28 +1,25 @@
 
- /**
+/**
  * USB Power Delivery integration in ESPHome
- *	
+ *
  * Authors: groothuisss & IMMRMKW
  */
 
-
 #include "fusb302.h"
-
-
+#define PIN_LED 27
 namespace esphome {
 namespace fusb302 {
 
-static const char *TAG = "fusb302.component";
+    static const char* TAG = "fusb302.component";
 
-class PD_UFP_c PD_UFP;
+    class PD_UFP_c PD_UFP;
 
+    void FUSB302::setup()
+    {
+        Wire.begin();
 
-void FUSB302::setup() {
-    Wire.begin();
-    
-    auto maxPowerSettingPDProtocol = PD_POWER_OPTION_MAX_5V;
-
-    switch (maximum_supply_voltage_) {
+        auto maxPowerSettingPDProtocol = PD_POWER_OPTION_MAX_5V;
+        switch (maximum_supply_voltage_) {
         case PD5V:
             maxPowerSettingPDProtocol = PD_POWER_OPTION_MAX_5V;
             break;
@@ -38,40 +35,55 @@ void FUSB302::setup() {
         case PD20V:
             maxPowerSettingPDProtocol = PD_POWER_OPTION_MAX_20V;
             break;
-    }
-	PD_UFP.set_fusb302_int_pin(this->interrupt_pin_->get_pin());
+        }
 
-    PD_UFP.init(maxPowerSettingPDProtocol);
-}
+        PD_UFP.set_fusb302_int_pin(this->interrupt_pin_->get_pin());
 
-void FUSB302::loop() {
-    PD_UFP.run();
-}
+        PD_UFP.init(maxPowerSettingPDProtocol);
 
-void FUSB302::dump_config(){
-    ESP_LOGCONFIG(TAG, "FUSB302:");
+        unsigned long timer = millis();
+        while (!PD_UFP.is_PPS_ready() && millis() - timer < TIME_OUT) {
+            PD_UFP.run();
+        }
 
-    switch (maximum_supply_voltage_) {
-    case PD5V:
-        ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 5V");
-        break;
-    case PD9V:
-        ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 9V");
-        break;
-    case PD12V:
-        ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 12V");
-        break;
-    case PD15V:
-        ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 15V");
-        break;
-    case PD20V:
-        ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 20V");
-        break;
+        timer = millis();
+        while (!PD_UFP.is_power_ready() && millis() - timer < TIME_OUT) {
+            PD_UFP.run();
+        }
+        if (!PD_UFP.is_PPS_ready() && !PD_UFP.is_power_ready()) {
+            ESP_LOGD(TAG, "ERROR: Neither PPS_ready or power_ready returned True.");
+        }
     }
 
-    LOG_PIN("  Interrupt pin: ", this->interrupt_pin_);
-}
+    void FUSB302::update()
+    {
+        PD_UFP.run();
+    }
 
+    void FUSB302::dump_config()
+    {
+        ESP_LOGCONFIG(TAG, "FUSB302:");
 
-}  // namespace fusb302
-}  // namespace esphome
+        switch (maximum_supply_voltage_) {
+        case PD5V:
+            ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 5V");
+            break;
+        case PD9V:
+            ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 9V");
+            break;
+        case PD12V:
+            ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 12V");
+            break;
+        case PD15V:
+            ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 15V");
+            break;
+        case PD20V:
+            ESP_LOGCONFIG(TAG, "  Maximum supply voltage: 20V");
+            break;
+        }
+
+        LOG_PIN("  Interrupt pin: ", this->interrupt_pin_);
+    }
+
+} // namespace fusb302
+} // namespace esphome
