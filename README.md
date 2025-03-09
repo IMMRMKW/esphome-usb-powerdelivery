4952744fcff7a2151490882c725f9cb0d91a017f
IMMRMKW <marcel_welleweerd@hotmail.com>
2025-03-09 18:54:53 +0100

    changed setup priority in __init__.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # esphome-usb-powerdelivery
-E Integration of the FUSB302 power delivery controller in ESPHome based on work of Groothuisss
+Integration of the FUSB302 power delivery controller in ESPHome based on work of Groothuisss. Developed for the [Home Automation Mate](https://immrmkw.github.io/HA_Mate/).
--- components/fusb302/__init__.py
+++ components/fusb302/__init__.py
@@ -8,10 +8,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
+from esphome.components import i2c
 from esphome.const import (
     CONF_ID,
     CONF_INTERRUPT_PIN,
+    CONF_SETUP_PRIORITY
 )
+
+DEPENDENCIES = ["i2c"]
 
 CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE = 'maximum_supply_voltage'
 
@@ -32,12 +36,14 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FUSB302),
     cv.Required(CONF_INTERRUPT_PIN): cv.All(pins.internal_gpio_input_pin_schema),
     cv.Optional(CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE, default="5v"): cv.enum(MAXIMUM_SUPPLY_VOLTAGE_OPTIONS, lower=True),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Optional(CONF_SETUP_PRIORITY, default = 900.0): cv.float_range(-100.0,1000.0),
+}).extend(i2c.i2c_device_schema(0x22)).extend(cv.polling_component_schema("10s"))
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
 
     if CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE in config:
         max_supply_voltage = config[CONF_OPTIONAL_MAX_SUPPLY_VOLTAGE]
